02170d2cb8ac6043a41334fe5f41ea8073fbab35
Yilong Li <mondaycha@outlook.com>
2024-01-19 19:11:38 +0800

    ci: update debug mode mysql port

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # go mod tidy && go mod vendor
-go build -mod=vendor -o bin/controller main.go
+export GIN_MODE=release && go build -mod=vendor -o bin/controller main.go
--- debug.sh
+++ debug.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # 运行
-go run main.go --db-config-file ./debug-dbconf.yaml --server-port :8099 --metrics-bind-address :8097 --health-probe-bind-address :8096 --config-file etc/debug-config.yaml
+export GIN_MODE=debug && go run main.go --db-config-file ./debug-dbconf.yaml --server-port :8099 --metrics-bind-address :8097 --health-probe-bind-address :8096 --config-file etc/debug-config.yaml
