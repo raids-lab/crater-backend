f35cfdf9e584fa25b2616235e60a25c064e99004
Yilong Li <mondaycha@outlook.com>
2024-01-24 18:45:00 +0800

    feat(wip): jupyter task

--- pkg/models/aitask.go
+++ pkg/models/aitask.go
@@ -96,15 +96,22 @@ type TaskAttr struct {
 	StartedAt time.Time `gorm:"column:started_at" json:"startedAt"`
 }
 
+type JupyterTaskAttr struct {
+	TaskName        string                `json:"taskName" binding:"required"`
+	Image           string                `json:"image" binding:"required"`
+	ResourceRequest v1.ResourceList       `json:"resourceRequest" binding:"required"`
+	ShareDirs       map[string][]DirMount `json:"shareDirs"`
+}
+
 type TaskStatusCount struct {
 	Status string
 	Count  int
 }
 
-// type Volume struct {
-// 	Name string `json:"name"`
-// 	Mounts []DirMount `json:"mounts"`
-// }
+//	type Volume struct {
+//		Name string `json:"name"`
+//		Mounts []DirMount `json:"mounts"`
+//	}
 type DirMount struct {
 	Volume    string `json:"volume"`
 	MountPath string `json:"mountPath"`
