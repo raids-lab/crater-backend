27716bb786ebc40520881368adf970902d1b8ae5
Yilong Li <mondaycha@outlook.com>
2024-03-18 15:26:09 +0800

    ci: cache go modules in .cache

--- debug.sh
+++ debug.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 export KUBECONFIG=${PWD}/kubeconfig
 go run main.go \
-    --db-config-file ./debug-dbconf.yaml \
     --config-file ./etc/debug-config.yaml \
-    --metrics-bind-address :8097 \
-    --health-probe-bind-address :8096 \
     --server-port :8099
