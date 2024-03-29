59370d524a22b086ce372141f5aa8813945893ae
Guangzu Wang <3331561613@qq.com>
2024-03-29 17:54:19 +0800

    wgz-refactor-args-parse

--- test/config_test.go
+++ test/config_test.go
@@ -1,13 +1,15 @@
-package config
+package test
 
 import (
 	"fmt"
 	"testing"
+
+	config2 "github.com/raids-lab/crater/pkg/config"
 )
 
 func TestYamlConfig(_ *testing.T) {
 	// 请替换为你的 Prometheus API 地址
-	config, err := NewConfig("")
+	config, err := config2.InitConfig()
 	fmt.Println(config)
 	fmt.Println(err)
 }
