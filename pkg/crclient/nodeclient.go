3a713a8ee4f98bc06a7b04563da52b5c770d12f0
unique021203 <liuxinwendexiaowo@163.com>
2024-03-31 13:38:05 +0000

    feat: cluster pod info

--- pkg/crclient/nodeclient.go
+++ pkg/crclient/nodeclient.go
@@ -2,7 +2,9 @@ package crclient
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/raids-lab/crater/pkg/monitor"
 	"github.com/raids-lab/crater/pkg/server/payload"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -12,34 +14,8 @@ import (
 
 type NodeClient struct {
 	client.Client
-	KubeClient *kubernetes.Clientset
-}
-
-func calculateAllocatedResources(node *corev1.Node, clientset *kubernetes.Clientset) corev1.ResourceList {
-	allocatedResources := make(corev1.ResourceList)
-	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
-	}
-
-	for i := range pods.Items {
-		pod := &pods.Items[i]
-		if pod.Spec.NodeName == node.Name {
-			for j := range pod.Spec.Containers {
-				container := &pod.Spec.Containers[j]
-				for resource, quantity := range container.Resources.Requests {
-					if allocatedQuantity, found := allocatedResources[resource]; found {
-						allocatedQuantity.Add(quantity)
-						allocatedResources[resource] = allocatedQuantity
-					} else {
-						allocatedResources[resource] = quantity.DeepCopy()
-					}
-				}
-			}
-		}
-	}
-
-	return allocatedResources
+	KubeClient  *kubernetes.Clientset
+	PromeClient *monitor.PrometheusClient
 }
 
 // https://stackoverflow.com/questions/67630551/how-to-use-client-go-to-get-the-node-status
@@ -62,6 +38,21 @@ func getNodeRole(node *corev1.Node) string {
 	return "worker"
 }
 
+func calculateCPULoad(ratio float32, cpu int) string {
+	// 计算当前CPU使用量
+	loadScaleFactor := 10
+	load := int(ratio * float32(cpu) * float32(loadScaleFactor))
+	// 将计算结果转换为字符串，并在末尾加上'm'
+	result := fmt.Sprintf("%dm", load)
+	return result
+}
+
+func FomatMemoryLoad(mem int) string {
+	mem_ := mem / 1024
+	result := fmt.Sprintf("%dMi", mem_)
+	return result
+}
+
 // GetNodes 获取所有 Node 列表
 func (nc *NodeClient) ListNodes() ([]payload.ClusterNodeInfo, error) {
 	var nodes corev1.NodeList
@@ -72,20 +63,106 @@ func (nc *NodeClient) ListNodes() ([]payload.ClusterNodeInfo, error) {
 	}
 
 	nodeInfos := make([]payload.ClusterNodeInfo, len(nodes.Items))
+	CPUMap := nc.PromeClient.QueryNodeCPUUsageRatio()
+	MemMap := nc.PromeClient.QueryNodeAllocatedMemory()
 
 	// Loop through each node and print allocated resources
 	for i := range nodes.Items {
 		node := &nodes.Items[i]
-		allocatedResources := calculateAllocatedResources(node, nc.KubeClient)
+		allocatedInfo := payload.AllocatedInfo{
+			CPU: calculateCPULoad(CPUMap[node.Name], int(node.Status.Capacity.Cpu().MilliValue())),
+			Mem: FomatMemoryLoad(MemMap[node.Name]),
+		}
+
+		nodeInfos[i] = payload.ClusterNodeInfo{
+			Name:      node.Name,
+			Role:      getNodeRole(node),
+			Labels:    node.Labels,
+			IsReady:   isNodeReady(node),
+			Capacity:  node.Status.Capacity,
+			Allocated: allocatedInfo,
+		}
+	}
+
+	return nodeInfos, nil
+}
+
+func (nc *NodeClient) ListNodesPod(name string) (payload.ClusterNodePodInfo, error) {
+	var nodes corev1.NodeList
+
+	err := nc.List(context.Background(), &nodes)
+	if err != nil {
+		return payload.ClusterNodePodInfo{}, err
+	}
+
+	for i := range nodes.Items {
+		node := &nodes.Items[i]
+		if node.Name != name {
+			continue
+		}
+		// 初始化节点信息
+		nodeInfo := payload.ClusterNodePodInfo{
+			Name:                    node.Name,
+			Role:                    getNodeRole(node),
+			IsReady:                 isNodeReady(node),
+			Time:                    node.CreationTimestamp.String(),
+			Address:                 node.Status.Addresses[0].Address,
+			Os:                      node.Status.NodeInfo.OperatingSystem,
+			OsVersion:               node.Status.NodeInfo.OSImage,
+			Arch:                    node.Status.NodeInfo.Architecture,
+			KubeletVersion:          node.Status.NodeInfo.KubeletVersion,
+			ContainerRuntimeVersion: node.Status.NodeInfo.ContainerRuntimeVersion,
+			Pods:                    []payload.Pod{},
+		}
+
+		// 使用KubeClient获取当前节点上的所有Pods
+		podList, err := nc.KubeClient.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{
+			FieldSelector: "spec.nodeName=" + node.Name,
+		})
+		if err != nil {
+			return payload.ClusterNodePodInfo{}, err
+		}
+		fmt.Print(podList.Items)
+
+		// 遍历当前节点的Pods，收集所需信息
+		for i := range podList.Items {
+			pod := &podList.Items[i]
+			podInfo := payload.Pod{
+				Name:       pod.Name,
+				IP:         pod.Status.PodIP,
+				CreateTime: pod.CreationTimestamp.String(),
+				Status:     string(pod.Status.Phase),
+				CPU:        nc.PromeClient.QueryPodCPURatio(pod.Name),
+				Mem:        FomatMemoryLoad(nc.PromeClient.QueryPodMemory(pod.Name)),
+			}
+
+			nodeInfo.Pods = append(nodeInfo.Pods, podInfo)
+		}
+
+		return nodeInfo, nil
+	}
+
+	return payload.ClusterNodePodInfo{}, fmt.Errorf("node %s not found", name)
+}
+
+func (nc *NodeClient) ListNodesTest() ([]payload.ClusterNodeInfo, error) {
+	var nodes corev1.NodeList
+
+	err := nc.List(context.Background(), &nodes)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeInfos := make([]payload.ClusterNodeInfo, len(nodes.Items))
+	for i := range nodes.Items {
+		node := &nodes.Items[i]
 		nodeInfos[i] = payload.ClusterNodeInfo{
 			Name:     node.Name,
 			Role:     getNodeRole(node),
 			Labels:   node.Labels,
 			IsReady:  isNodeReady(node),
 			Capacity: node.Status.Capacity,
-			Alocated: allocatedResources,
 		}
 	}
-
 	return nodeInfos, nil
 }
--- pkg/monitor/prometheus_test.go
+++ pkg/monitor/prometheus_test.go
@@ -2,6 +2,7 @@ package monitor
 
 import (
 	"fmt"
+	"log"
 	"testing"
 )
 
@@ -9,15 +10,27 @@ func TestQueryPodUtilMetric(t *testing.T) {
 	// 请替换为你的 Prometheus API 地址
 	apiURL := ***REMOVED***
 	client := NewPrometheusClient(apiURL)
-
-	// 请替换为你的测试 namespace 和 pod 名称
-	namespace := "jupyter"
-	podname := "img2img-train"
-
-	podUtil, err := client.QueryPodUtilMetric(namespace, podname)
+	// print url
+	fmt.Println(apiURL)
+	podUtil, err := client.queryMetric("kube_gpu_scheduler_pod_bind_gpu")
 	if err != nil {
 		t.Errorf("Error querying PodUtilMetric: %v", err)
 		return
 	}
 	fmt.Printf("PodUtilMetric: %+v\n", podUtil)
+}
+
+func TestQueryPodUtilMetric2(t *testing.T) {
+	// 请替换为你的 Prometheus API 地址
+	apiURL := ***REMOVED***
+	client := NewPrometheusClient(apiURL)
+
+	// print url
+	fmt.Println(apiURL)
+	expression := "kube_gpu_scheduler_pod_bind_gpu"
+	err := client.QueryMetricDemo(expression)
+	if err != nil {
+		log.Fatalf("queryMetric error: %v", err)
+		t.Errorf("Error querying PodUtilMetric: %v", err)
+	}
 }
