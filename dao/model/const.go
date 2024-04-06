20960dc2ed905f095c386c44cad7c972b7681a57
Yilong Li <mondaycha@outlook.com>
2024-04-06 03:24:56 +0000

    Dev

--- dao/model/const.go
+++ dao/model/const.go
@@ -22,16 +22,16 @@ const (
 type AccessMode uint8
 
 const (
-	AccessModeRW AccessMode = iota // Read-write mode
-	AccessModeRO                   // Read-only mode
+	AccessModeRO AccessMode = iota // Read-only mode
 	AccessModeAO                   // Append-only mode
+	AccessModeRW                   // Read-write mode
 )
 
 // Job status
 type JobStatus uint8
 
 const (
-	JobInitial   JobStatus = iota // 初始状态，未通过配额检查
+	JobInitial   JobStatus = iota // 初始状态，未进行或未通过配额检查
 	JobCreated                    // 作业已通过配额检查，提交到集群中，等待调度
 	JobRunning                    // 作业正在运行
 	JobSucceeded                  // 作业的所有 Pod 均成功完成
--- dao/model/user.go
+++ dao/model/user.go
@@ -10,7 +10,7 @@ type User struct {
 	Name         string  `gorm:"uniqueIndex;type:varchar(32);not null;comment:用户名"`
 	Nickname     *string `gorm:"type:varchar(32);comment:昵称"`
 	Password     *string `gorm:"type:varchar(128);comment:密码"`
-	Role         Role    `gorm:"index:role;not null;comment:用户在平台的角色 (admin, user, guest)"`
+	Role         Role    `gorm:"index:role;not null;comment:用户在平台的角色 (guest, user, admin)"`
 	Status       Status  `gorm:"index:status;not null;comment:用户状态 (pending, active, inactive)"`
 	UserProjects []UserProject
 }
@@ -19,7 +19,9 @@ type UserProject struct {
 	gorm.Model
 	UserID    uint `gorm:"primaryKey"`
 	ProjectID uint `gorm:"primaryKey"`
-	Role      Role `gorm:"not null;comment:用户在项目中的角色 (admin, user)"`
+	Role      Role `gorm:"not null;comment:用户在项目中的角色 (guest, user, admin)"`
+
+	AccessMode AccessMode `gorm:"not null;default:0;comment:项目空间的访问模式 (ro, ao, rw)"`
 
 	// quota (job, node, cpu, memory, gpu, gpuMem, storage) for the project
 	// same as Quota
@@ -38,8 +40,8 @@ type UserProject struct {
 	MemReq int `gorm:"type:int;not null;default:0;comment:可以提交的内存配额 (Gi)"`
 	Mem    int `gorm:"type:int;not null;default:0;comment:可以同时使用的内存配额 (Gi)"`
 
-	GPUMemReq int `gorm:"type:int;not null;default:-1;comment:可以提交的GPU内存配额 (Gi)"`
-	GPUMem    int `gorm:"type:int;not null;default:-1;comment:可以同时使用的GPU内存配额 (Gi)"`
+	GPUMemReq int `gorm:"type:int;not null;default:-1;comment:可以提交的 GPU 内存配额 (Gi)"`
+	GPUMem    int `gorm:"type:int;not null;default:-1;comment:可以同时使用的 GPU 内存配额 (Gi)"`
 
 	Storage int `gorm:"type:int;not null;default:50;comment:存储配额 (Gi)"`
 
