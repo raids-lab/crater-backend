af85c512552cb3405ff92580527ecc973a9d631c
hjx620 <huangjx@buaa.edu.cn>
2024-04-09 13:26:00 +0000

    fix(context):quota limit in project

--- internal/handler/context.go
+++ internal/handler/context.go
@@ -36,11 +36,59 @@ func (mgr *ContextMgr) GetQuota(c *gin.Context) {
 	var quota payload.Quota
 	err = up.WithContext(c).Where(up.ProjectID.Eq(token.ProjectID), up.UserID.Eq(token.UserID)).Select(up.ALL).Scan(&quota)
 	if err != nil {
-		resputil.Error(c, fmt.Sprintf("find quota failed, detail: %v", err), resputil.NotSpecified)
+		resputil.Error(c, fmt.Sprintf("find quota of user in project failed, detail: %v", err), resputil.NotSpecified)
 		return
 	}
+	q := query.Quota
+	var quotaInProject payload.Quota
+	err = q.WithContext(c).Where(q.ProjectID.Eq(token.ProjectID)).Select(q.ALL).Scan(&quotaInProject)
+	if err != nil {
+		resputil.Error(c, fmt.Sprintf("find quota of project failed, detail: %v", err), resputil.NotSpecified)
+		return
+	}
+	newQuota := QuotaLimitOrNot(&quota, &quotaInProject)
 
-	resputil.Success(c, quota)
+	resputil.Success(c, *newQuota)
 
 	// 获取当前用户当前项目的Quota
+}
+func QuotaLimitOrNot(quota, quotaInProject *payload.Quota) *payload.Quota {
+	const nolimit = -1
+	if quota.CPU == nolimit {
+		quota.CPU = quotaInProject.CPU
+	}
+	if quota.CPUReq == nolimit {
+		quota.CPUReq = quotaInProject.CPUReq
+	}
+	if quota.GPU == nolimit {
+		quota.GPU = quotaInProject.GPU
+	}
+	if quota.GPUReq == nolimit {
+		quota.GPUReq = quotaInProject.GPUReq
+	}
+	if quota.GPUMemReq == nolimit {
+		quota.GPUMemReq = quotaInProject.GPUMemReq
+	}
+	if quota.GPUMem == nolimit {
+		quota.GPUMem = quotaInProject.GPUMem
+	}
+	if quota.Node == nolimit {
+		quota.Node = quotaInProject.Node
+	}
+	if quota.NodeReq == nolimit {
+		quota.NodeReq = quotaInProject.NodeReq
+	}
+	if quota.Job == nolimit {
+		quota.Job = quotaInProject.Job
+	}
+	if quota.JobReq == nolimit {
+		quota.JobReq = quotaInProject.JobReq
+	}
+	if quota.Node == nolimit {
+		quota.Node = quotaInProject.Node
+	}
+	if quota.NodeReq == nolimit {
+		quota.NodeReq = quotaInProject.NodeReq
+	}
+	return quota
 }
