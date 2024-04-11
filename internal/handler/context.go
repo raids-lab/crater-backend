1579c7603acac4b9516f9f4d3b1956c0ac1ce40d
hjx620 <huangjx@buaa.edu.cn>
2024-04-11 17:39:15 +0800

    refactor:quota

--- internal/handler/context.go
+++ internal/handler/context.go
@@ -2,10 +2,11 @@ package handler
 
 import (
 	"fmt"
+	"reflect"
 
 	"github.com/gin-gonic/gin"
+	"github.com/raids-lab/crater/dao/model"
 	"github.com/raids-lab/crater/dao/query"
-	"github.com/raids-lab/crater/internal/payload"
 	"github.com/raids-lab/crater/internal/util"
 	resputil "github.com/raids-lab/crater/pkg/server/response"
 )
@@ -26,6 +27,17 @@ func (mgr *ContextMgr) RegisterProtected(g *gin.RouterGroup) {
 
 func (mgr *ContextMgr) RegisterAdmin(_ *gin.RouterGroup) {}
 
+// GetQuota godoc
+// @Summary 获取当前用户当前项目的Quota
+// @Description 如果 UserProject 表没有对 Quota 进行限制，则返回项目的 Quota
+// @Tags Context
+// @Accept json
+// @Produce json
+// @Security Bearer
+// @Success 200 {object} resputil.Response[model.EmbeddedQuota] "配额信息"
+// @Failure 400 {object} resputil.Response[any] "请求参数错误"
+// @Failure 500 {object} resputil.Response[any] "其他错误"
+// @Router /v1/context/quota [get]
 func (mgr *ContextMgr) GetQuota(c *gin.Context) {
 	token, err := util.GetToken(c)
 	if err != nil {
@@ -33,62 +45,44 @@ func (mgr *ContextMgr) GetQuota(c *gin.Context) {
 		return
 	}
 	up := query.UserProject
-	var quota payload.Quota
+	var quota model.EmbeddedQuota
 	err = up.WithContext(c).Where(up.ProjectID.Eq(token.ProjectID), up.UserID.Eq(token.UserID)).Select(up.ALL).Scan(&quota)
 	if err != nil {
 		resputil.Error(c, fmt.Sprintf("find quota of user in project failed, detail: %v", err), resputil.NotSpecified)
 		return
 	}
 	q := query.Quota
-	var quotaInProject payload.Quota
+
+	var quotaInProject model.EmbeddedQuota
 	err = q.WithContext(c).Where(q.ProjectID.Eq(token.ProjectID)).Select(q.ALL).Scan(&quotaInProject)
 	if err != nil {
 		resputil.Error(c, fmt.Sprintf("find quota of project failed, detail: %v", err), resputil.NotSpecified)
 		return
 	}
-	newQuota := QuotaLimitOrNot(&quota, &quotaInProject)
+
+	newQuota := getUserQuota(&quota, &quotaInProject)
 
 	resputil.Success(c, *newQuota)
-
-	// 获取当前用户当前项目的Quota
 }
-func QuotaLimitOrNot(quota, quotaInProject *payload.Quota) *payload.Quota {
-	const nolimit = -1
-	if quota.CPU == nolimit {
-		quota.CPU = quotaInProject.CPU
+
+// 获取用户的配额
+// 如果用户在项目中有配额限制，则返回用户在项目中的配额
+// 否则返回项目的配额
+func getUserQuota(quota, quotaInProject *model.EmbeddedQuota) *model.EmbeddedQuota {
+	quotaValue := reflect.ValueOf(quota).Elem()
+	projectValue := reflect.ValueOf(quotaInProject).Elem()
+
+	for i := 0; i < quotaValue.NumField(); i++ {
+		field := quotaValue.Field(i)
+		if field.Interface() == model.Unlimited {
+			// 对于 Unlimited 的字段，使用项目的对应字段
+			field.Set(projectValue.Field(i))
+		} else if field.Type() == reflect.TypeOf((*string)(nil)) {
+			// 对于指针类型的字段，如果为 nil，使用项目的对应字段
+			field.Set(projectValue.Field(i))
+		}
 	}
-	if quota.CPUReq == nolimit {
-		quota.CPUReq = quotaInProject.CPUReq
-	}
-	if quota.GPU == nolimit {
-		quota.GPU = quotaInProject.GPU
-	}
-	if quota.GPUReq == nolimit {
-		quota.GPUReq = quotaInProject.GPUReq
-	}
-	if quota.GPUMemReq == nolimit {
-		quota.GPUMemReq = quotaInProject.GPUMemReq
-	}
-	if quota.GPUMem == nolimit {
-		quota.GPUMem = quotaInProject.GPUMem
-	}
-	if quota.Node == nolimit {
-		quota.Node = quotaInProject.Node
-	}
-	if quota.NodeReq == nolimit {
-		quota.NodeReq = quotaInProject.NodeReq
-	}
-	if quota.Job == nolimit {
-		quota.Job = quotaInProject.Job
-	}
-	if quota.JobReq == nolimit {
-		quota.JobReq = quotaInProject.JobReq
-	}
-	if quota.Node == nolimit {
-		quota.Node = quotaInProject.Node
-	}
-	if quota.NodeReq == nolimit {
-		quota.NodeReq = quotaInProject.NodeReq
-	}
+
 	return quota
+
 }
--- internal/handler/project.go
+++ internal/handler/project.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -10,15 +11,23 @@ import (
 	"github.com/raids-lab/crater/dao/query"
 	"github.com/raids-lab/crater/internal/payload"
 	"github.com/raids-lab/crater/internal/util"
+	"github.com/raids-lab/crater/pkg/aitaskctl"
 	"github.com/raids-lab/crater/pkg/config"
+	"github.com/raids-lab/crater/pkg/logutils"
+	"github.com/raids-lab/crater/pkg/models"
 	resputil "github.com/raids-lab/crater/pkg/server/response"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 )
 
 type ProjectMgr struct {
+	taskController *aitaskctl.TaskController
 }
 
-func NewProjectMgr() Manager {
-	return &ProjectMgr{}
+func NewProjectMgr(taskController *aitaskctl.TaskController) Manager {
+	return &ProjectMgr{
+		taskController: taskController,
+	}
 }
 
 func (mgr *ProjectMgr) RegisterPublic(_ *gin.RouterGroup) {}
@@ -30,6 +39,7 @@ func (mgr *ProjectMgr) RegisterProtected(g *gin.RouterGroup) {
 
 func (mgr *ProjectMgr) RegisterAdmin(g *gin.RouterGroup) {
 	g.GET("", mgr.ListAllForAdmin) // 获取所有项目
+	g.PUT("/:name/quotas", mgr.UpdateQuota)
 }
 
 // ListAllForUser godoc
@@ -201,9 +211,10 @@ func (mgr *ProjectMgr) CreateTeamProject(c *gin.Context) {
 
 		// Create a user-project relationship without quota limit
 		userProject := model.UserProject{
-			UserID:    token.UserID,
-			ProjectID: project.ID,
-			Role:      model.RoleAdmin, // Set the user as the admin
+			UserID:        token.UserID,
+			ProjectID:     project.ID,
+			Role:          model.RoleAdmin, // Set the user as the admin
+			EmbeddedQuota: model.QuotaUnlimited,
 		}
 		if err := up.WithContext(c).Create(&userProject); err != nil {
 			return err
@@ -221,8 +232,10 @@ func (mgr *ProjectMgr) CreateTeamProject(c *gin.Context) {
 		}
 
 		// Create a quota for the personal project
-		quota := model.DefaultQuota()
-		quota.ProjectID = project.ID
+		quota := model.Quota{
+			ProjectID:     project.ID,
+			EmbeddedQuota: model.QuotaDefault,
+		}
 		quota.CPUReq = *req.Quota.CPU
 		quota.CPU = *req.Quota.CPU
 		quota.MemReq = *req.Quota.Memory
@@ -245,4 +258,102 @@ func (mgr *ProjectMgr) CreateTeamProject(c *gin.Context) {
 	} else {
 		resputil.Success(c, ProjectCreateResp{ID: projectID})
 	}
+}
+
+type UpdateQuotaReq struct {
+	JobReq *int `json:"jobReq"`
+	Job    *int `json:"job"`
+
+	NodeReq *int `json:"nodeReq"`
+	Node    *int `json:"node"`
+
+	CPUReq *int `json:"cpuReq"`
+	CPU    *int `json:"cpu"`
+
+	GPUReq *int `json:"gpuReq"`
+	GPU    *int `json:"gpu"`
+
+	MemReq *int `json:"memReq"`
+	Mem    *int `json:"mem"`
+
+	GPUMemReq *int `json:"gpuMemReq"`
+	GPUMem    *int `json:"gpuMem"`
+
+	Storage *int    `json:"storage"`
+	Extra   *string `json:"extra"`
+}
+
+type ProjectNameReq struct {
+	Name string `uri:"name" binding:"required"`
+}
+
+// UpdateQuota godoc
+// @Summary 更新配额
+// @Description 更新配额
+// @Tags User
+// @Accept json
+// @Produce json
+// @Security Bearer
+// @Param name path ProjectNameReq true "projectname"
+// @Param data body UpdateQuotaReq true "更新quota"
+// @Success 200 {object} resputil.Response[string] "成功更新配额"
+// @Failure 400 {object} resputil.Response[any] "请求参数错误"
+// @Failure 500 {object} resputil.Response[any] "其他错误"
+// @Router /v1/admin/projects/{name}/quotas [put]
+func (mgr *ProjectMgr) UpdateQuota(c *gin.Context) {
+	var req UpdateQuotaReq
+	var nameReq ProjectNameReq
+	if err := c.ShouldBindJSON(&req); err != nil {
+		resputil.Error(c, fmt.Sprintf("validate update parameters failed, detail: %v", err), resputil.NotSpecified)
+		return
+	}
+	if err := c.ShouldBindUri(&nameReq); err != nil {
+		resputil.Error(c, fmt.Sprintf("validate update parameters failed, detail: %v", err), resputil.NotSpecified)
+		return
+	}
+	name := nameReq.Name
+	p := query.Project
+	_, err := p.WithContext(c).Where(p.Name.Eq(name)).Updates(map[string]any{
+		"job_req":     *req.JobReq,
+		"job":         *req.Job,
+		"node_req":    *req.NodeReq,
+		"node":        *req.Node,
+		"cpu_req":     *req.CPUReq,
+		"cpu":         *req.CPU,
+		"gpu_req":     *req.GPUReq,
+		"gpu":         *req.GPU,
+		"mem_req":     *req.MemReq,
+		"mem":         *req.Mem,
+		"gpu_mem_req": *req.GPUMemReq,
+		"gpu_mem":     *req.GPUMem,
+		"storage":     *req.Storage,
+		"extra":       req.Extra,
+	})
+	if err != nil {
+		resputil.Error(c, fmt.Sprintf("update quota failed, detail: %v", err), resputil.NotSpecified)
+		return
+	}
+	project, err := p.WithContext(c).Where(p.Name.Eq(name)).First()
+
+	if err != nil {
+		resputil.Error(c, fmt.Sprintf("find project failed, detail: %v", err), resputil.NotSpecified)
+		return
+	}
+
+	ProjectID := strconv.FormatUint(uint64(project.ID), 10)
+
+	q1 := v1.ResourceList{
+		v1.ResourceCPU:                    *resource.NewScaledQuantity(int64(*req.CPU), resource.Milli),
+		v1.ResourceMemory:                 *resource.NewScaledQuantity(int64(*req.Mem), resource.Giga),
+		v1.ResourceName("nvidia.com/gpu"): *resource.NewQuantity(int64(*req.GPU), resource.DecimalSI),
+	}
+	oldquota := models.Quota{
+		UserName:  name,
+		NameSpace: config.GetConfig().Workspace.Namespace,
+		HardQuota: models.ResourceListToJSON(q1),
+	}
+	mgr.taskController.AddOrUpdateQuotaInfo(ProjectID, &oldquota)
+
+	logutils.Log.Infof("update quota success, project: %s, quota:%v", name, oldquota.HardQuota)
+	resputil.Success(c, "")
 }
