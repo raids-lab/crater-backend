3d86d860697c03a6feecedd91e84073900e16ac4
Yilong Li <mondaycha@outlook.com>
2024-04-11 14:59:00 +0800

    refactor: quota embedded in project

--- dao/model/project.go
+++ dao/model/project.go
@@ -4,14 +4,18 @@ import "gorm.io/gorm"
 
 type Project struct {
 	gorm.Model
-	Name         string  `gorm:"uniqueIndex;type:varchar(48);not null;comment:项目名"`
-	Description  *string `gorm:"type:varchar(512);comment:项目描述"`
-	Namespace    string  `gorm:"type:varchar(128);not null;comment:命名空间"`
-	Status       Status  `gorm:"index:status;not null;comment:项目状态 (pending, active, inactive)"`
-	IsPersonal   bool    `gorm:"type:boolean;not null;comment:是否为个人项目"`
+	Name        string  `gorm:"uniqueIndex;type:varchar(48);not null;comment:项目名"`
+	Description *string `gorm:"type:varchar(512);comment:项目描述"`
+	Namespace   string  `gorm:"type:varchar(128);not null;comment:命名空间"`
+	Status      Status  `gorm:"index:status;not null;comment:项目状态 (pending, active, inactive)"`
+	IsPersonal  bool    `gorm:"type:boolean;not null;comment:是否为个人项目"`
+
 	UserProjects []UserProject
 	// Project can also associate with multiple spaces in RW or RO mode
 	ProjectSpaces []ProjectSpace
+
+	// Project level quota
+	EmbeddedQuota `gorm:"embedded"`
 }
 
 type ProjectSpace struct {
