4f19c51cc49b681c51f1b504eb2c4d6f07c4fdd3
huangsy <1845930603@qq.com>
2024-04-16 07:48:10 +0000

    fix: 修复镜像相关bug

--- internal/handler/imagepack.go
+++ internal/handler/imagepack.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -18,27 +19,44 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type ImagePackCreateRequest struct {
-	GitRepository   string              `json:"gitRepository"`
-	AccessToken     string              `json:"accessToken"`
-	RegistryServer  string              `json:"registryServer"`
-	RegistryUser    string              `json:"registryUser"`
-	RegistryPass    string              `json:"registryPass"`
-	RegistryProject string              `json:"registryProject"`
-	ImageName       string              `json:"imageName"`
-	ImageTag        string              `json:"imageTag"`
-	NeedProfile     bool                `json:"needProfile"`
-	TaskType        model.ImageTaskType `json:"taskType"`
-}
+type (
+	ImagePackCreateRequest struct {
+		GitRepository   string              `json:"gitRepository"`
+		AccessToken     string              `json:"accessToken"`
+		RegistryServer  string              `json:"registryServer"`
+		RegistryUser    string              `json:"registryUser"`
+		RegistryPass    string              `json:"registryPass"`
+		RegistryProject string              `json:"registryProject"`
+		ImageName       string              `json:"imageName"`
+		ImageTag        string              `json:"imageTag"`
+		NeedProfile     bool                `json:"needProfile"`
+		TaskType        model.ImageTaskType `json:"taskType"`
+	}
 
-type ImagePackDeleteByIDRequest struct {
-	ID uint `json:"id"`
-}
+	ImagePackDeleteByIDRequest struct {
+		ID uint `json:"id"`
+	}
 
-type ImagePackParamsUpdateRequest struct {
-	ImagePackName string                   `json:"imagepackname"`
-	Data          model.ImageProfileParams `json:"data"`
-}
+	ImagePackParamsUpdateRequest struct {
+		ImagePackName string                   `json:"imagepackname"`
+		Data          model.ImageProfileParams `json:"data"`
+	}
+	ImagePackAdminListRequest struct {
+		Type int `form:"type"`
+	}
+)
+
+type (
+	ImagePackListResponse struct {
+		ID          uint                     `json:"ID"`
+		ImageLink   string                   `json:"imagelink"`
+		Status      string                   `json:"status"`
+		CreatedAt   time.Time                `json:"createdAt"`
+		NameTag     string                   `json:"nametag"`
+		CreaterName string                   `json:"creatername"`
+		Params      model.ImageProfileParams `json:"params"`
+	}
+)
 
 const (
 	UserNameSpace     = "crater-jobs"
@@ -58,8 +76,8 @@ type ImagePackMgr struct {
 }
 
 func (mgr *ImagePackMgr) RegisterProtected(g *gin.RouterGroup) {
-	g.GET("/list", mgr.ListAll)
-	g.POST("/create", mgr.Create)
+	g.GET("/list", mgr.UserListAll)
+	g.POST("/create", mgr.UserCreate)
 	g.POST("/delete", mgr.DeleteByID)
 	g.GET("/available", mgr.ListAvailableImages)
 	g.POST("/params", mgr.UpdateParams)
@@ -67,8 +85,8 @@ func (mgr *ImagePackMgr) RegisterProtected(g *gin.RouterGroup) {
 }
 
 func (mgr *ImagePackMgr) RegisterAdmin(g *gin.RouterGroup) {
-	g.GET("/list", mgr.ListAll)
-	g.POST("/create", mgr.Create)
+	g.GET("/list", mgr.AdminListAll)
+	g.POST("/create", mgr.AdminCreate)
 	g.POST("/delete", mgr.DeleteByID)
 }
 
@@ -81,7 +99,7 @@ func NewImagePackMgr(
 	}
 }
 
-// createImagePack godoc
+// UserCreate godoc
 // @Summary 创建ImagePack CRD和数据库entity
 // @Description 获取参数，生成变量，调用接口
 // @Tags ImagePack
@@ -89,8 +107,8 @@ func NewImagePackMgr(
 // @Produce json
 // @Security Bearer
 // @Param data body ImagePackCreateRequest true "创建ImagePack"
-// @Router /image/create [post]
-func (mgr *ImagePackMgr) Create(c *gin.Context) {
+// @Router /images/create [post]
+func (mgr *ImagePackMgr) UserCreate(c *gin.Context) {
 	req := &ImagePackCreateRequest{}
 	token, _ := util.GetToken(c)
 	if err := c.ShouldBindJSON(req); err != nil {
@@ -101,7 +119,31 @@ func (mgr *ImagePackMgr) Create(c *gin.Context) {
 	}
 	logutils.Log.Infof("create params: %+v", req)
 	mgr.requestDefaultValue(req)
-	mgr.createImagePack(c, req, token)
+	mgr.createImagePack(c, req, token, token.ProjectID)
+	resputil.Success(c, "")
+}
+
+// AdminCreate godoc
+// @Summary 创建ImagePack CRD和数据库entity
+// @Description 获取参数，生成变量，调用接口
+// @Tags ImagePack
+// @Accept json
+// @Produce json
+// @Security Bearer
+// @Param data body ImagePackCreateRequest true "创建ImagePack"
+// @Router /images/create [post]
+func (mgr *ImagePackMgr) AdminCreate(c *gin.Context) {
+	req := &ImagePackCreateRequest{}
+	token, _ := util.GetToken(c)
+	if err := c.ShouldBindJSON(req); err != nil {
+		msg := fmt.Sprintf("validate create parameters failed, err %v", err)
+		logutils.Log.Errorf(msg)
+		resputil.HTTPError(c, http.StatusBadRequest, msg, resputil.NotSpecified)
+		return
+	}
+	logutils.Log.Infof("create params: %+v", req)
+	mgr.requestDefaultValue(req)
+	mgr.createImagePack(c, req, token, 1)
 	resputil.Success(c, "")
 }
 
@@ -114,14 +156,20 @@ func (mgr *ImagePackMgr) requestDefaultValue(req *ImagePackCreateRequest) {
 	}
 }
 
-func (mgr *ImagePackMgr) createImagePack(c *gin.Context, req *ImagePackCreateRequest, token util.ReqContext) {
+func (mgr *ImagePackMgr) createImagePack(c *gin.Context, req *ImagePackCreateRequest, token util.ReqContext, projectID uint) {
 	userQuery := query.User
-	imagepackQuery := query.Image
 	user, err := userQuery.WithContext(c).Where(userQuery.ID.Eq(token.UserID)).First()
 	if err != nil {
 		logutils.Log.Errorf("fetch user failed, params: %+v err:%v", req, err)
 		return
 	}
+	projectQuery := query.Project
+	project, err := projectQuery.WithContext(c).Where(projectQuery.ID.Eq(token.ProjectID)).First()
+	if err != nil {
+		logutils.Log.Errorf("fetch project failed, params: %+v err:%v", req, err)
+		return
+	}
+	imagepackQuery := query.Image
 	imagepackName := fmt.Sprintf("%s-%s", user.Name, uuid.New().String())
 	// create ImagePack CRD
 	imagepackCRD := &imagepackv1.ImagePack{
@@ -146,15 +194,12 @@ func (mgr *ImagePackMgr) createImagePack(c *gin.Context, req *ImagePackCreateReq
 		logutils.Log.Errorf("create imagepack CRD failed, params: %+v err:%v", imagepackCRD, err)
 		return
 	}
-	ProjectID := token.ProjectID
-	// create ImagePack DataBase entity
-	if token.PlatformRole == model.RoleAdmin {
-		ProjectID = PublicProjectID
-	}
 	imageLink := fmt.Sprintf("%s/%s/%s:%s", req.RegistryServer, req.RegistryProject, req.ImageName, req.ImageTag)
 	imagepackEntity := &model.Image{
 		UserID:        token.UserID,
-		ProjectID:     ProjectID,
+		User:          *user,
+		ProjectID:     projectID,
+		Project:       *project,
 		ImagePackName: imagepackName,
 		ImageLink:     imageLink,
 		NameSpace:     UserNameSpace,
@@ -170,58 +215,97 @@ func (mgr *ImagePackMgr) createImagePack(c *gin.Context, req *ImagePackCreateReq
 	}
 }
 
-// ListAll godoc
+// UserListAll godoc
 // @Summary 用户和管理员获取相关镜像的功能
-// @Description 如果userRole为user，则返回该用户所有的镜像；否则根据GET参数type来决定搜索私有or公共镜像
+// @Description 返回该用户所有的镜像
 // @Tags ImagePack
 // @Accept json
 // @Produce json
 // @Security Bearer
 // @Param type query int true "管理员获取镜像的类型"
-// @Router /image/list [GET]
-func (mgr *ImagePackMgr) ListAll(c *gin.Context) {
+// @Router /images/list [GET]
+func (mgr *ImagePackMgr) UserListAll(c *gin.Context) {
 	imagepackQuery := query.Image
 	token, _ := util.GetToken(c)
 	var imagepacks []*model.Image
 	var err error
-	if token.ClusterRole == model.RoleUser {
-		imagepacks, err = imagepackQuery.WithContext(c).Where(imagepackQuery.ProjectID.Eq(token.ProjectID)).Find()
-	} else if token.ClusterRole == model.RoleAdmin {
-		listType := c.DefaultQuery("type", "0")
-		if listType == "0" {
-			imagepacks, err = imagepackQuery.WithContext(c).Where(
-				imagepackQuery.ProjectID.Neq(PublicProjectID),
-			).Where(
-				imagepackQuery.Status.Neq(ImagePackDeleted),
-			).Find()
-		} else if listType == "1" {
-			imagepacks, err = imagepackQuery.WithContext(c).Where(
-				imagepackQuery.ProjectID.Eq(PublicProjectID),
-			).Where(
-				imagepackQuery.Status.Neq(ImagePackDeleted),
-			).Find()
-		} else {
-			logutils.Log.Errorf("list image type error, err:%v", err)
-			resputil.Error(c, "list image type error", resputil.NotSpecified)
-			return
-		}
-		if err != nil {
-			logutils.Log.Errorf("admin fetch personal or public imagepack failed, err:%v", err)
-			resputil.Error(c, "list image type error", resputil.NotSpecified)
-			return
-		}
-	}
-
+	imagepacks, err = imagepackQuery.WithContext(c).Where(imagepackQuery.ProjectID.Eq(token.ProjectID)).Find()
 	if err != nil {
 		logutils.Log.Errorf("fetch imagepacks entity failed, err:%v", err)
 	}
+	responses := []ImagePackListResponse{}
 	for i := range imagepacks {
 		imagepack := imagepacks[i]
 		if imagepack.Status != ImagePackFinished && imagepack.Status != ImagePackFailed {
 			mgr.updateImagePackStatus(c, imagepack)
 		}
+		responses = append(responses, mgr.generateImageListResponse(imagepack))
 	}
-	resputil.Success(c, imagepacks)
+	resputil.Success(c, responses)
+}
+
+// AdminListAll godoc
+// @Summary 管理员获取相关镜像的功能
+// @Description 根据GET参数type来决定搜索私有or公共镜像
+// @Tags ImagePack
+// @Accept json
+// @Produce json
+// @Security Bearer
+// @Param type query int true "管理员获取镜像的类型"
+// @Router /images/list [GET]
+func (mgr *ImagePackMgr) AdminListAll(c *gin.Context) {
+	imagepackQuery := query.Image
+	var imagepacks []*model.Image
+	var err error
+	var req ImagePackAdminListRequest
+	if err = c.ShouldBindQuery(&req); err != nil {
+		msg := fmt.Sprintf("validate list parameters failed, err %v", err)
+		logutils.Log.Errorf(msg)
+		resputil.HTTPError(c, http.StatusBadRequest, msg, resputil.NotSpecified)
+		return
+	}
+	listType := req.Type
+	if listType == 0 {
+		imagepacks, err = imagepackQuery.WithContext(c).
+			Where(imagepackQuery.ProjectID.Neq(PublicProjectID)).
+			Where(imagepackQuery.Status.Neq(ImagePackDeleted)).
+			Find()
+	} else if listType == 1 {
+		imagepacks, err = imagepackQuery.WithContext(c).
+			Where(imagepackQuery.ProjectID.Eq(PublicProjectID)).
+			Where(imagepackQuery.Status.Neq(ImagePackDeleted)).
+			Find()
+	} else {
+		logutils.Log.Errorf("admin list image type error, err:%v", err)
+		resputil.Error(c, "admin list image type error", resputil.NotSpecified)
+		return
+	}
+	if err != nil {
+		logutils.Log.Errorf("admin fetch personal or public imagepack failed, err:%v", err)
+		resputil.Error(c, "list image type error", resputil.NotSpecified)
+		return
+	}
+	responses := []ImagePackListResponse{}
+	for i := range imagepacks {
+		imagepack := imagepacks[i]
+		if imagepack.Status != ImagePackFinished && imagepack.Status != ImagePackFailed {
+			mgr.updateImagePackStatus(c, imagepack)
+		}
+		responses = append(responses, mgr.generateImageListResponse(imagepack))
+	}
+	resputil.Success(c, responses)
+}
+
+func (mgr *ImagePackMgr) generateImageListResponse(imagepack *model.Image) ImagePackListResponse {
+	return ImagePackListResponse{
+		ID:          imagepack.ID,
+		ImageLink:   imagepack.ImageLink,
+		Status:      imagepack.Status,
+		CreatedAt:   imagepack.CreatedAt,
+		NameTag:     imagepack.NameTag,
+		CreaterName: imagepack.User.Name,
+		Params:      imagepack.Params,
+	}
 }
 
 func (mgr *ImagePackMgr) updateImagePackStatus(c *gin.Context, imagepack *model.Image) {
@@ -254,7 +338,7 @@ func (mgr *ImagePackMgr) updateImagePackStatus(c *gin.Context, imagepack *model.
 // @Accept json
 // @Produce json
 // @Security Bearer
-// @Router /image/available [GET]
+// @Router /images/available [GET]
 func (mgr *ImagePackMgr) ListAvailableImages(c *gin.Context) {
 	token, _ := util.GetToken(c)
 	imagepackQuery := query.Image
@@ -287,7 +371,7 @@ func (mgr *ImagePackMgr) ListAvailableImages(c *gin.Context) {
 // @Produce json
 // @Security Bearer
 // @Param ID body uint true "删除镜像的ID"
-// @Router /image/delete [POST]
+// @Router /images/delete [POST]
 func (mgr *ImagePackMgr) DeleteByID(c *gin.Context) {
 	token, _ := util.GetToken(c)
 	imagepackQuery := query.Image
@@ -326,7 +410,7 @@ func (mgr *ImagePackMgr) DeleteByID(c *gin.Context) {
 // @Produce json
 // @Security Bearer
 // @Param data body ImagePackParamsUpdateRequest true "更新ImagePack的name"
-// @Router /image/params [post]
+// @Router /images/params [post]
 func (mgr *ImagePackMgr) UpdateParams(c *gin.Context) {
 	imagePackParamsUpdateRequest := &ImagePackParamsUpdateRequest{}
 	imagepackQuery := query.Image
@@ -358,7 +442,7 @@ func (mgr *ImagePackMgr) UpdateParams(c *gin.Context) {
 // @Produce json
 // @Security Bearer
 // @Param imagepackname query string true "获取ImagePack的name"
-// @Router /image/get [GET]
+// @Router /images/get [GET]
 func (mgr *ImagePackMgr) GetImagePack(c *gin.Context) {
 	imagepackQuery := query.Image
 	imagePackName := c.DefaultQuery("imagepackname", "")
