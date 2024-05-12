dca354f29d8ff48d894056f4d1479f1a0962bd5c
huangsy <1845930603@qq.com>
2024-05-12 06:57:58 +0000

    fix: 修复available接口

--- dao/model/imagepack.go
+++ dao/model/imagepack.go
@@ -6,8 +6,8 @@ type Image struct {
 	gorm.Model
 	UserID        uint
 	User          User
-	ProjectID     uint
-	Project       Project
+	QueueID       uint
+	Queue         Queue
 	ImagePackName string             `gorm:"column:imagepackname;uniqueIndex:imagepackname;type:varchar(128);not null" json:"imagepackname"`
 	ImageLink     string             `gorm:"column:imagelink;type:varchar(128);not null" json:"imagelink"`
 	NameSpace     string             `gorm:"column:namespace;type:varchar(128);not null" json:"namespace"`
@@ -16,6 +16,9 @@ type Image struct {
 	Params        ImageProfileParams `gorm:"column:params;type:varchar(512);serializer:json;" json:"params"`
 	NeedProfile   bool               `gorm:"column:needprofile;type:boolean;default:false" json:"needprofile"`
 	TaskType      ImageTaskType      `gorm:"column:tasktype;not null;comment:作业状态" json:"tasktype"`
+	Alias         string             `gorm:"column:alias;type:varchar(128);not null" json:"alias"`
+	Description   string             `gorm:"column:description;type:varchar(512);not null" json:"description"`
+	CreatorName   string             `gorm:"column:creatorname;type:varchar(128);not null" json:"creatorname"`
 }
 
 type ImageProfileParams struct {
