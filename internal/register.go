20afcd9bfc8536e2be721058a74f59ccdd7730fe
Yilong Li <mondaycha@outlook.com>
2024-10-20 09:57:39 +0800

    refactor: manager register

---
+++ internal/register.go
@@ -0,0 +1,22 @@
+package internal
+
+import (
+	"github.com/raids-lab/crater/internal/handler"
+	_ "github.com/raids-lab/crater/internal/handler/aijob"
+	_ "github.com/raids-lab/crater/internal/handler/operations"
+	_ "github.com/raids-lab/crater/internal/handler/spjob"
+	_ "github.com/raids-lab/crater/internal/handler/tool"
+	_ "github.com/raids-lab/crater/internal/handler/vcjob"
+	"github.com/raids-lab/crater/pkg/logutils"
+)
+
+// registerManagers registers all the managers.
+func registerManagers(config handler.RegisterConfig) []handler.Manager {
+	var managers []handler.Manager
+	for _, register := range handler.Registers {
+		manager := register(config)
+		managers = append(managers, manager)
+		logutils.Log.Infof("Registered manager: %s", manager.GetName())
+	}
+	return managers
+}
