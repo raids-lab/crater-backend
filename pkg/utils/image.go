1fc29cf058c28e47e2f2552bfae6447359298767
Yilong Li <mondaycha@outlook.com>
2024-12-18 20:39:29 +0800

    fix: time tzdata

--- pkg/utils/image.go
+++ pkg/utils/image.go
@@ -5,6 +5,9 @@ import (
 	"regexp"
 	"time"
 
+	// reference: https://stackoverflow.com/questions/60103251/time-loadlocation-works-regularly-but-throws-an-error-on-my-docker-instance-how
+	_ "time/tzdata"
+
 	"github.com/google/uuid"
 	"github.com/raids-lab/crater/pkg/config"
 )
@@ -31,9 +34,12 @@ func GenerateNewImageLink(imageLink, username string) (newImageLink string, err 
 	}
 	registryServer := config.GetConfig().ACT.Image.RegistryServer
 	registryProject := fmt.Sprintf("user-%s", username)
-	loc, _ := time.LoadLocation("Asia/Shanghai")
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		return "", err
+	}
 	now := time.Now().In(loc)
-	imageTag := fmt.Sprintf("%d%d-%d%d-%s", now.Month(), now.Day(), now.Hour(), now.Minute(), uuid.New().String()[:4])
+	imageTag := fmt.Sprintf("%02d%02d%02d%02d-%s", now.Month(), now.Day(), now.Hour(), now.Minute(), uuid.New().String()[:4])
 	newImageLink = fmt.Sprintf("%s/%s/%s:%s", registryServer, registryProject, imageName, imageTag)
 	return newImageLink, nil
 }
