1b8b14886578c589bbb3f3c97835fc1b4550b35e
Yilong Li <mondaycha@outlook.com>
2024-12-25 21:47:39 +0800

    fix: nerdctl commit with retry

--- hack/install-nerdctl.sh
+++ hack/install-nerdctl.sh
@@ -2,10 +2,10 @@
 set -e
 
 # Usage: bash hack/install-nerdctl.sh
-NERDCTL_VERSION=2.0.1
+NERDCTL_VERSION=2.0.1-retry
 REGISTERY=***REMOVED***
 
-wget https://github.com/containerd/nerdctl/releases/download/v2.0.1/nerdctl-2.0.1-linux-amd64.tar.gz -O bin/nerdctl-2.0.1-linux-amd64.tar.gz
+# wget https://github.com/containerd/nerdctl/releases/download/v2.0.1/nerdctl-2.0.1-linux-amd64.tar.gz -O bin/nerdctl-2.0.1-linux-amd64.tar.gz
 
 tar -zxf bin/nerdctl-2.0.1-linux-amd64.tar.gz -C bin
 
@@ -13,3 +13,5 @@ docker build -t $REGISTERY/nerdctl:$NERDCTL_VERSION -f deployments/nerdctl/Docke
 
 # rm bin/nerdctl-2.0.1-linux-amd64.tar.gz
 # rm bin/nerdctl
+
+docker push $REGISTERY/nerdctl:$NERDCTL_VERSION
