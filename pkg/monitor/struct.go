087d429b1f6f5ae4f5bc53a46b9e61ce1088b349
Yilong Li <mondaycha@outlook.com>
2025-03-06 20:47:00 +0800

    feat: profile data

--- pkg/monitor/struct.go
+++ pkg/monitor/struct.go
@@ -25,42 +25,42 @@ type NodeGPUUtil struct {
 
 type ProfileData struct {
 	// CPU and Memory
-	CPUUsageAvg *float32 `json:"cpu_usage_avg"`
-	CPUUsageMax *float32 `json:"cpu_usage_max"`
-	CPUUsageStd *float32 `json:"cpu_usage_std"`
+	CPUUsageAvg *float32 `json:"cpu_usage_avg,omitempty"`
+	CPUUsageMax *float32 `json:"cpu_usage_max,omitempty"`
+	CPUUsageStd *float32 `json:"cpu_usage_std,omitempty"`
 
-	CPUMemAvg *float32 `json:"cpu_mem_avg"`
-	CPUMemMax *float32 `json:"cpu_mem_max"`
-	CPUMemStd *float32 `json:"cpu_mem_std"`
+	CPUMemAvg *float32 `json:"cpu_mem_avg,omitempty"`
+	CPUMemMax *float32 `json:"cpu_mem_max,omitempty"`
+	CPUMemStd *float32 `json:"cpu_mem_std,omitempty"`
 
 	// GPU
-	GPUUtilAvg *float32 `json:"gpu_util_avg"`
-	GPUUtilMax *float32 `json:"gpu_util_max"`
-	GPUUtilStd *float32 `json:"gpu_util_std"`
+	GPUUtilAvg *float32 `json:"gpu_util_avg,omitempty"`
+	GPUUtilMax *float32 `json:"gpu_util_max,omitempty"`
+	GPUUtilStd *float32 `json:"gpu_util_std,omitempty"`
 
-	SMActiveAvg *float32 `json:"sm_active_avg"`
-	SMActiveMax *float32 `json:"sm_active_max"`
-	SMActiveStd *float32 `json:"sm_active_std"`
+	SMActiveAvg *float32 `json:"sm_active_avg,omitempty"`
+	SMActiveMax *float32 `json:"sm_active_max,omitempty"`
+	SMActiveStd *float32 `json:"sm_active_std,omitempty"`
 
-	SMOccupancyAvg *float32 `json:"sm_occupancy_avg"`
-	SMOccupancyMax *float32 `json:"sm_occupancy_max"`
-	SMOccupancyStd *float32 `json:"sm_occupancy_std"`
+	SMOccupancyAvg *float32 `json:"sm_occupancy_avg,omitempty"`
+	SMOccupancyMax *float32 `json:"sm_occupancy_max,omitempty"`
+	SMOccupancyStd *float32 `json:"sm_occupancy_std,omitempty"`
 
-	DramUtilAvg *float32 `json:"dram_util_avg"`
-	DramUtilMax *float32 `json:"dram_util_max"`
-	DramUtilStd *float32 `json:"dram_util_std"`
+	DramUtilAvg *float32 `json:"dram_util_avg,omitempty"`
+	DramUtilMax *float32 `json:"dram_util_max,omitempty"`
+	DramUtilStd *float32 `json:"dram_util_std,omitempty"`
 
-	MemCopyUtilAvg *float32 `json:"mem_copy_util_avg"`
-	MemCopyUtilMax *float32 `json:"mem_copy_util_max"`
-	MemCopyUtilStd *float32 `json:"mem_copy_util_std"`
+	MemCopyUtilAvg *float32 `json:"mem_copy_util_avg,omitempty"`
+	MemCopyUtilMax *float32 `json:"mem_copy_util_max,omitempty"`
+	MemCopyUtilStd *float32 `json:"mem_copy_util_std,omitempty"`
 
-	PCIETxAvg *float32 `json:"pcie_tx_avg"`
-	PCIETxMax *float32 `json:"pcie_tx_max"`
+	PCIETxAvg *float32 `json:"pcie_tx_avg,omitempty"`
+	PCIETxMax *float32 `json:"pcie_tx_max,omitempty"`
 
-	PCIERxAvg *float32 `json:"pcie_rx_avg"`
-	PCIERxMax *float32 `json:"pcie_rx_max"`
+	PCIERxAvg *float32 `json:"pcie_rx_avg,omitempty"`
+	PCIERxMax *float32 `json:"pcie_rx_max,omitempty"`
 
-	GPUMemMax *float32 `json:"gpu_mem_max"`
+	GPUMemMax *float32 `json:"gpu_mem_max,omitempty"`
 }
 
 type PodUtil struct {
