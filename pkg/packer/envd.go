7f40087e034f475dd11f29b612e7ebf6988ebc78
spiner-z <spinerzhang@gmail.com>
2025-07-09 02:41:37 +0000

    fix: no pod clean for waiting jupyter

--- pkg/packer/envd.go
+++ pkg/packer/envd.go
@@ -5,11 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 
-	"github.com/raids-lab/crater/pkg/config"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
+
+	"github.com/raids-lab/crater/pkg/config"
 )
 
 func (b *imagePacker) CreateFromEnvd(c context.Context, data *EnvdReq) error {
